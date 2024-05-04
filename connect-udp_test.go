302a6a00f883f5e8b65a307b173345afe92eabeb
Marten Seemann <martenseemann@gmail.com>
2024-05-04 11:23:08 +0800

    add Close method to client and server, check for Goroutine leaks

--- connect-udp_test.go
+++ connect-udp_test.go
@@ -7,40 +7,100 @@ import (
 	"net"
 	"net/http"
 	"testing"
-	"time"
 
 	"github.com/quic-go/masque-go"
 
 	"github.com/quic-go/quic-go"
 	"github.com/quic-go/quic-go/http3"
+
 	"github.com/stretchr/testify/require"
 	"github.com/yosida95/uritemplate/v3"
+	"go.uber.org/goleak"
 )
 
-func TestProxy(t *testing.T) {
-	remoteServerConn, err := net.ListenUDP("udp", &net.UDPAddr{IP: net.IPv4(127, 0, 0, 1), Port: 0})
+func TestMain(m *testing.M) {
+	goleak.VerifyTestMain(m)
+}
+
+func runEchoServer(t *testing.T) *net.UDPConn {
+	t.Helper()
+	conn, err := net.ListenUDP("udp", &net.UDPAddr{IP: net.IPv4(127, 0, 0, 1), Port: 0})
 	require.NoError(t, err)
 	go func() {
 		for {
 			b := make([]byte, 1500)
-			n, addr, err := remoteServerConn.ReadFrom(b)
+			n, addr, err := conn.ReadFrom(b)
 			if err != nil {
 				return
 			}
-			if _, err := remoteServerConn.WriteTo(b[:n], addr); err != nil {
+			if _, err := conn.WriteTo(b[:n], addr); err != nil {
 				return
 			}
 		}
 	}()
+	return conn
+}
+
+func TestProxying(t *testing.T) {
+	remoteServerConn := runEchoServer(t)
 	defer remoteServerConn.Close()
 
-	laddr, err := net.ResolveUDPAddr("udp", "127.0.0.1:0")
+	conn, err := net.ListenUDP("udp", &net.UDPAddr{IP: net.IPv4(127, 0, 0, 1), Port: 0})
 	require.NoError(t, err)
-	conn, err := net.ListenUDP("udp", laddr)
-	require.NoError(t, err)
+	defer conn.Close()
 	t.Logf("server listening on %s", conn.LocalAddr())
-	template, err := uritemplate.New(fmt.Sprintf("https://localhost:%d/masque?h={target_host}&p={target_port}", conn.LocalAddr().(*net.UDPAddr).Port))
+	template := uritemplate.MustNew(fmt.Sprintf("https://localhost:%d/masque?h={target_host}&p={target_port}", conn.LocalAddr().(*net.UDPAddr).Port))
+
+	mux := http.NewServeMux()
+	server := masque.Server{
+		Server: http3.Server{
+			TLSConfig:       tlsConf,
+			QUICConfig:      &quic.Config{EnableDatagrams: true},
+			EnableDatagrams: true,
+			Handler:         mux,
+		},
+		Template: template,
+		Allow:    func(context.Context, *net.UDPAddr) bool { return true },
+	}
+	defer server.Close()
+	mux.HandleFunc("/masque", func(w http.ResponseWriter, r *http.Request) {
+		if err := server.Upgrade(w, r); err != nil {
+			t.Log("Upgrade failed:", err)
+			return
+		}
+		w.WriteHeader(http.StatusOK)
+	})
+	go func() {
+		if err := server.Serve(conn); err != nil {
+			return
+		}
+	}()
+
+	cl := masque.Client{
+		Template:        template,
+		TLSClientConfig: &tls.Config{ClientCAs: certPool, NextProtos: []string{http3.NextProtoH3}, InsecureSkipVerify: true},
+	}
+	defer cl.Close()
+	proxiedConn, err := cl.DialIP(context.Background(), remoteServerConn.LocalAddr().(*net.UDPAddr))
 	require.NoError(t, err)
+
+	_, err = proxiedConn.WriteTo([]byte("foobar"), remoteServerConn.LocalAddr())
+	require.NoError(t, err)
+	b := make([]byte, 1500)
+	n, _, err := proxiedConn.ReadFrom(b)
+	require.NoError(t, err)
+	require.Equal(t, []byte("foobar"), b[:n])
+}
+
+func TestProxyShutdown(t *testing.T) {
+	remoteServerConn := runEchoServer(t)
+	defer remoteServerConn.Close()
+
+	conn, err := net.ListenUDP("udp", &net.UDPAddr{IP: net.IPv4(127, 0, 0, 1), Port: 0})
+	require.NoError(t, err)
+	defer conn.Close()
+	t.Logf("server listening on %s", conn.LocalAddr())
+	template := uritemplate.MustNew(fmt.Sprintf("https://localhost:%d/masque?h={target_host}&p={target_port}", conn.LocalAddr().(*net.UDPAddr).Port))
 
 	mux := http.NewServeMux()
 	server := masque.Server{
@@ -62,7 +122,7 @@ func TestProxy(t *testing.T) {
 	})
 	go func() {
 		if err := server.Serve(conn); err != nil {
-			panic(err)
+			return
 		}
 	}()
 
@@ -70,16 +130,20 @@ func TestProxy(t *testing.T) {
 		Template:        template,
 		TLSClientConfig: &tls.Config{ClientCAs: certPool, NextProtos: []string{http3.NextProtoH3}, InsecureSkipVerify: true},
 	}
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
-	proxiedConn, err := cl.DialIP(ctx, remoteServerConn.LocalAddr().(*net.UDPAddr))
+	defer cl.Close()
+	proxiedConn, err := cl.DialIP(context.Background(), remoteServerConn.LocalAddr().(*net.UDPAddr))
 	require.NoError(t, err)
 
 	_, err = proxiedConn.WriteTo([]byte("foobar"), remoteServerConn.LocalAddr())
 	require.NoError(t, err)
-
 	b := make([]byte, 1500)
 	n, _, err := proxiedConn.ReadFrom(b)
 	require.NoError(t, err)
 	require.Equal(t, []byte("foobar"), b[:n])
+
+	// Close the server and expect the proxied connection to unblock ReadFrom and WriteTo.
+	server.Close()
+	_, _, err = proxiedConn.ReadFrom(b)
+	require.Error(t, err)
+	// TODO: check that WriteTo errors as well, needs https://github.com/quic-go/quic-go/issues/4485
 }
