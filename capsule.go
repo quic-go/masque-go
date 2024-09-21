ce793aad8ab80400d4275458a90659cb3436d865
Marten Seemann <martenseemann@gmail.com>
2024-09-21 14:10:24 +0800

    implement parsing of the ADDRESS_REQUEST capsule

--- capsule.go
+++ capsule.go
@@ -10,7 +10,10 @@ import (
 	"github.com/quic-go/quic-go/quicvarint"
 )
 
-const capsuleTypeAddressAssign http3.CapsuleType = 1
+const (
+	capsuleTypeAddressAssign  http3.CapsuleType = 1
+	capsuleTypeAddressRequest http3.CapsuleType = 2
+)
 
 // addressAssignCapsule represents an ADDRESS_ASSIGN capsule
 type addressAssignCapsule struct {
@@ -23,58 +26,84 @@ type AssignedAddress struct {
 	IPPrefix  netip.Prefix
 }
 
+// addressRequestCapsule represents an ADDRESS_REQUEST capsule
+type addressRequestCapsule struct {
+	RequestedAddresses []RequestedAddress
+}
+
+// RequestedAddress represents an Requested Address within an ADDRESS_REQUEST capsule
+type RequestedAddress struct {
+	RequestID uint64
+	IPPrefix  netip.Prefix
+}
+
 func parseAddressAssignCapsule(r io.Reader) (*addressAssignCapsule, error) {
 	var assignedAddresses []AssignedAddress
 	for {
-		addr, err := parseAssignedAddress(r)
+		requestID, prefix, err := parseAddress(r)
 		if err != nil {
 			if err == io.EOF {
 				break
 			}
 			return nil, err
 		}
-		assignedAddresses = append(assignedAddresses, addr)
+		assignedAddresses = append(assignedAddresses, AssignedAddress{RequestID: requestID, IPPrefix: prefix})
 	}
 	return &addressAssignCapsule{AssignedAddresses: assignedAddresses}, nil
 }
 
-func parseAssignedAddress(r io.Reader) (AssignedAddress, error) {
+func parseAddressRequestCapsule(r io.Reader) (*addressRequestCapsule, error) {
+	var requestedAddresses []RequestedAddress
+	for {
+		requestID, prefix, err := parseAddress(r)
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			return nil, err
+		}
+		requestedAddresses = append(requestedAddresses, RequestedAddress{RequestID: requestID, IPPrefix: prefix})
+	}
+	return &addressRequestCapsule{RequestedAddresses: requestedAddresses}, nil
+}
+
+func parseAddress(r io.Reader) (requestID uint64, prefix netip.Prefix, _ error) {
 	vr := quicvarint.NewReader(r)
 	requestID, err := quicvarint.Read(vr)
 	if err != nil {
-		return AssignedAddress{}, err
+		return 0, netip.Prefix{}, err
 	}
 	ipVersion, err := vr.ReadByte()
 	if err != nil {
-		return AssignedAddress{}, err
+		return 0, netip.Prefix{}, err
 	}
 	var ip netip.Addr
 	switch ipVersion {
 	case 4:
 		var ipv4 [4]byte
 		if _, err := io.ReadFull(r, ipv4[:]); err != nil {
-			return AssignedAddress{}, err
+			return 0, netip.Prefix{}, err
 		}
 		ip = netip.AddrFrom4(ipv4)
 	case 6:
 		var ipv6 [16]byte
 		if _, err := io.ReadFull(r, ipv6[:]); err != nil {
-			return AssignedAddress{}, err
+			return 0, netip.Prefix{}, err
 		}
 		ip = netip.AddrFrom16(ipv6)
 	default:
-		return AssignedAddress{}, fmt.Errorf("invalid IP version: %d", ipVersion)
+		return 0, netip.Prefix{}, fmt.Errorf("invalid IP version: %d", ipVersion)
 	}
 	prefixLen, err := vr.ReadByte()
 	if err != nil {
-		return AssignedAddress{}, err
+		return 0, netip.Prefix{}, err
 	}
 	if int(prefixLen) > ip.BitLen() {
-		return AssignedAddress{}, fmt.Errorf("prefix length %d exceeds IP address length (%d)", prefixLen, ip.BitLen())
+		return 0, netip.Prefix{}, fmt.Errorf("prefix length %d exceeds IP address length (%d)", prefixLen, ip.BitLen())
 	}
-	prefix := netip.PrefixFrom(ip, int(prefixLen))
+	prefix = netip.PrefixFrom(ip, int(prefixLen))
 	if prefix != prefix.Masked() {
-		return AssignedAddress{}, errors.New("lower bits not covered by prefix length are not all zero")
+		return 0, netip.Prefix{}, errors.New("lower bits not covered by prefix length are not all zero")
 	}
-	return AssignedAddress{RequestID: requestID, IPPrefix: prefix}, nil
+	return requestID, prefix, nil
 }
--- capsule_test.go
+++ capsule_test.go
@@ -44,19 +44,25 @@ func TestParseAddressAssignCapsule(t *testing.T) {
 }
 
 func TestParseAddressAssignCapsuleInvalid(t *testing.T) {
+	testParseAddressCapsuleInvalid(t, capsuleTypeAddressAssign, func(r io.Reader) error {
+		_, err := parseAddressAssignCapsule(quicvarint.NewReader(r))
+		return err
+	})
+}
+
+func testParseAddressCapsuleInvalid(t *testing.T, typ http3.CapsuleType, f func(r io.Reader) error) {
 	t.Run("invalid IP version", func(t *testing.T) {
 		addr1 := quicvarint.Append(nil, 1337) // Request ID
 		addr1 = append(addr1, 5)              // Invalid IP version (not 4 or 6)
 		addr1 = append(addr1, netip.AddrFrom4([4]byte{1, 2, 3, 4}).AsSlice()...)
 		addr1 = append(addr1, 32) // IP Prefix Length
-		data := quicvarint.Append(nil, uint64(capsuleTypeAddressAssign))
+		data := quicvarint.Append(nil, uint64(typ))
 		data = quicvarint.Append(data, uint64(len(addr1))) // Length
 		data = append(data, addr1...)
 
 		_, cr, err := http3.ParseCapsule(bytes.NewReader(data))
 		require.NoError(t, err)
-		_, err = parseAddressAssignCapsule(cr)
-		require.ErrorContains(t, err, "invalid IP version: 5")
+		require.ErrorContains(t, f(cr), "invalid IP version: 5")
 	})
 
 	t.Run("invalid prefix length", func(t *testing.T) {
@@ -64,14 +70,13 @@ func TestParseAddressAssignCapsuleInvalid(t *testing.T) {
 		addr1 = append(addr1, 4)              // IPv4
 		addr1 = append(addr1, netip.AddrFrom4([4]byte{1, 2, 3, 4}).AsSlice()...)
 		addr1 = append(addr1, 33) // too long IP Prefix Length
-		data := quicvarint.Append(nil, uint64(capsuleTypeAddressAssign))
+		data := quicvarint.Append(nil, uint64(typ))
 		data = quicvarint.Append(data, uint64(len(addr1))) // Length
 		data = append(data, addr1...)
 
 		_, cr, err := http3.ParseCapsule(bytes.NewReader(data))
 		require.NoError(t, err)
-		_, err = parseAddressAssignCapsule(cr)
-		require.ErrorContains(t, err, "prefix length 33 exceeds IP address length (32)")
+		require.ErrorContains(t, f(cr), "prefix length 33 exceeds IP address length (32)")
 	})
 
 	t.Run("lower bits not covered by prefix length are not all zero", func(t *testing.T) {
@@ -79,29 +84,32 @@ func TestParseAddressAssignCapsuleInvalid(t *testing.T) {
 		addr1 = append(addr1, 4)                                                 // IPv4
 		addr1 = append(addr1, netip.AddrFrom4([4]byte{1, 2, 3, 4}).AsSlice()...) // non-zero lower bits
 		addr1 = append(addr1, 28)                                                // IP Prefix Length
-		data := quicvarint.Append(nil, uint64(capsuleTypeAddressAssign))
+		data := quicvarint.Append(nil, uint64(typ))
 		data = quicvarint.Append(data, uint64(len(addr1))) // Length
 		data = append(data, addr1...)
 
 		_, cr, err := http3.ParseCapsule(bytes.NewReader(data))
 		require.NoError(t, err)
-		_, err = parseAddressAssignCapsule(cr)
-		require.ErrorContains(t, err, "lower bits not covered by prefix length are not all zero")
+		require.ErrorContains(t, f(cr), "lower bits not covered by prefix length are not all zero")
 	})
 
 	t.Run("incomplete capsule", func(t *testing.T) {
 		addr1 := quicvarint.Append(nil, 1337) // Request ID
 		addr1 = append(addr1, 4)              // IPv4
 		addr1 = append(addr1, netip.AddrFrom4([4]byte{1, 2, 3, 4}).AsSlice()...)
-		addr1 = append(addr1, 32) // IP Prefix Length
-		data := quicvarint.Append(nil, uint64(capsuleTypeAddressAssign))
-		data = quicvarint.Append(data, uint64(len(addr1))) // Length
+		addr1 = append(addr1, 32)             // IP Prefix Length
+		addr2 := quicvarint.Append(nil, 1338) // Request ID
+		addr2 = append(addr2, 6)              // IPv6
+		addr2 = append(addr2, netip.MustParseAddr("2001:db8::1").AsSlice()...)
+		addr2 = append(addr2, 128) // IP Prefix Length
+		data := quicvarint.Append(nil, uint64(typ))
+		data = quicvarint.Append(data, uint64(len(addr1)+len(addr2))) // Length
 		data = append(data, addr1...)
+		data = append(data, addr2...)
 
 		_, cr, err := http3.ParseCapsule(bytes.NewReader(data))
 		require.NoError(t, err)
-		_, err = parseAddressAssignCapsule(cr)
-		require.NoError(t, err)
+		require.NoError(t, f(cr))
 		for i := range data {
 			_, cr, err := http3.ParseCapsule(bytes.NewReader(data[:i]))
 			if err != nil {
@@ -115,5 +123,42 @@ func TestParseAddressAssignCapsuleInvalid(t *testing.T) {
 			_, err = parseAddressAssignCapsule(cr)
 			require.ErrorIs(t, err, io.ErrUnexpectedEOF)
 		}
+	})
+}
+
+func TestParseAddressRequestCapsule(t *testing.T) {
+	addr1 := quicvarint.Append(nil, 1337) // Request ID
+	addr1 = append(addr1, 4)              // IPv4
+	addr1 = append(addr1, netip.AddrFrom4([4]byte{1, 2, 3, 0}).AsSlice()...)
+	addr1 = append(addr1, 24)             // IP Prefix Length
+	addr2 := quicvarint.Append(nil, 1338) // Request ID
+	addr2 = append(addr2, 6)              // IPv6
+	addr2 = append(addr2, netip.MustParseAddr("2001:db8::1").AsSlice()...)
+	addr2 = append(addr2, 128) // IP Prefix Length
+	data := quicvarint.Append(nil, uint64(capsuleTypeAddressRequest))
+	data = quicvarint.Append(data, uint64(len(addr1)+len(addr2))) // Length
+	data = append(data, addr1...)
+	data = append(data, addr2...)
+
+	r := bytes.NewReader(data)
+	typ, cr, err := http3.ParseCapsule(r)
+	require.NoError(t, err)
+	require.Equal(t, capsuleTypeAddressRequest, typ)
+	capsule, err := parseAddressRequestCapsule(cr)
+	require.NoError(t, err)
+	require.Equal(t,
+		[]RequestedAddress{
+			{RequestID: 1337, IPPrefix: netip.MustParsePrefix("1.2.3.0/24")},
+			{RequestID: 1338, IPPrefix: netip.MustParsePrefix("2001:db8::1/128")},
+		},
+		capsule.RequestedAddresses,
+	)
+	require.Zero(t, r.Len())
+}
+
+func TestParseAddressRequestCapsuleInvalid(t *testing.T) {
+	testParseAddressCapsuleInvalid(t, capsuleTypeAddressRequest, func(r io.Reader) error {
+		_, err := parseAddressRequestCapsule(quicvarint.NewReader(r))
+		return err
 	})
 }
